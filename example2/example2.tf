948f4db00e68c1bbe0a2957af75b6299050b2477
naresh <n399naresh@gmail.com>
2018-03-18 16:16:15 -0400

    sixth2 commit

--- example2/example2.tf
+++ example2/example2.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "example" {
 }
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   ingress {
     from_port = 8080
     to_port = 8080
@@ -23,4 +24,3 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-vpc_security_group_ids = ["${aws_security_group.instance.id}"]
